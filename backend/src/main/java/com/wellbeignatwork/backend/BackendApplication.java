8e47ec75d18d4e57b96459eb529c40c4cea029fa
MortadhaGaied <mortadha.gaied@esprit.tn>
2022-03-28 02:25:17 +0100

    remove fileStorage

--- backend/src/main/java/com/wellbeignatwork/backend/BackendApplication.java
+++ backend/src/main/java/com/wellbeignatwork/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.wellbeignatwork.backend;
 
-import com.wellbeignatwork.backend.service.Forum.FileStorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,17 +10,12 @@ import javax.annotation.Resource;
 @EnableSwagger2
 @SpringBootApplication
 @EnableScheduling
-public class BackendApplication implements CommandLineRunner {
-    @Resource
-    FileStorageService storageService;
+public class BackendApplication  {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-    @Override
-    public void run(String... arg) throws Exception {
-        storageService.deleteAll();
-        storageService.init();
-    }
+
 
 
 }
