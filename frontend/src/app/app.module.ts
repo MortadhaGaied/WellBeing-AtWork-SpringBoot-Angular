9a53af9d7facc8da66e1cc8f8db99d5365033034
MortadhaGaied <mortadha.gaied@esprit.tn>
2022-04-10 00:35:32 +0100

    add header footer sidebar and homepage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomePageComponent } from './core/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
