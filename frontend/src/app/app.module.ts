029208e834f7396ea17da2a8249532daad221314
MortadhaGaied <mortadha.gaied@esprit.tn>
2022-04-10 01:46:39 +0100

    decompose homepage to many component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { HeaderComponent } from './shared/header/header.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomePageComponent } from './core/home-page/home-page.component';
+import { AdvertismentCarouselComponent } from './core/advertisment-carousel/advertisment-carousel.component';
+import { SponsordComponent } from './core/sponsord/sponsord.component';
+import { EventWidgetComponent } from './core/event-widget/event-widget.component';
+import { QaWidgetComponent } from './core/qa-widget/qa-widget.component';
+import { SharePostComponent } from './core/share-post/share-post.component';
+import { ChatRoomsComponent } from './core/chat-rooms/chat-rooms.component';
+import { StoriesComponent } from './core/stories/stories.component';
+import { CreatePostComponent } from './core/create-post/create-post.component';
+import { LinksComponent } from './core/links/links.component';
+import { RecentBlogComponent } from './core/recent-blog/recent-blog.component';
+import { CompleteProfileComponent } from './core/complete-profile/complete-profile.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +24,18 @@ import { HomePageComponent } from './core/home-page/home-page.component';
     HeaderComponent,
     SideBarComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    AdvertismentCarouselComponent,
+    SponsordComponent,
+    EventWidgetComponent,
+    QaWidgetComponent,
+    SharePostComponent,
+    ChatRoomsComponent,
+    StoriesComponent,
+    CreatePostComponent,
+    LinksComponent,
+    RecentBlogComponent,
+    CompleteProfileComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/core/advertisment-carousel/advertisment-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisment-carousel',
+  templateUrl: './advertisment-carousel.component.html',
+  styleUrls: ['./advertisment-carousel.component.css']
+})
+export class AdvertismentCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
