9467757ea25f50e879a7bcd0cc5773eb1333db5e
HomraniMahdi <mahdi.homrani@esprit.tn>
2022-04-30 16:36:50 +0100

    back

--- backoffice/src/app/collaboration/collaboration.module.ts
+++ backoffice/src/app/collaboration/collaboration.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CollaborationRoutingModule } from './collaboration-routing.module';
 import { CollaborationsComponent } from './collaborations/collaborations.component';
 import { AddCollaborationComponent } from './add-collaboration/add-collaboration.component';
+import { OfferComponent } from './offer/offer.component';
+import { AddOfferComponent } from './add-offer/add-offer.component';
 
 
 @NgModule({
   declarations: [
     CollaborationsComponent,
-    AddCollaborationComponent
+    AddCollaborationComponent,
+    OfferComponent,
+    AddOfferComponent
   ],
   imports: [
     CommonModule,
