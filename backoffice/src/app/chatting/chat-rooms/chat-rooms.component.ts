bbc03900b6ae7d74ac1d4760b8c3e043f01f61c1
Auckfmine <mouhamedaminerouatbi@gmail.com>
2022-05-03 03:20:43 +0100

    chatroom details

--- backoffice/src/app/chatting/chat-rooms/chat-rooms.component.ts
+++ backoffice/src/app/chatting/chat-rooms/chat-rooms.component.ts
@@ -19,50 +19,48 @@ export class ChatRoomsComponent implements OnInit {
   item$: Observable<any[]>;
   col: CollectionReference<DocumentData>;
   col2: CollectionReference<DocumentData>;
-  totalMessagesCount:number=0;
-  totalRoomsCount:number=5;
+  totalMessagesCount: number = 0;
+  totalRoomsCount: number = 0;
   constructor(private service: ChatroomService, private firestore: Firestore) {
     //this.col = collection(firestore, "items");
     this.col2 = collection(firestore, "top-chatters");
     this.item$ = collectionData(this.col2);
-
   }
-
 
   ngOnInit(): void {
     //this.item$.subscribe(data=>console.log(data));
     //this.getMessagesCount();
 
     this.getRoomCount();
-    console.log("rooms count : "+this.totalRoomsCount)
+    console.log("rooms count : " + this.totalRoomsCount);
   }
 
-
-  getMessagesCount():void{
-    this.service.getAllMessages().subscribe(num=>{
-      console.log(num)
-      this.topcards[0].title=num+''
-      this.totalMessagesCount=num});
+  getMessagesCount(): void {
+    this.service.getAllMessages().subscribe((num) => {
+      console.log(num);
+      this.topcards[0].title = num + "";
+      this.totalMessagesCount = num;
+    });
   }
 
-  getRoomCount():void{
-    this.service.getAllRooms().subscribe(rooms=>{
-      console.log(rooms.length)
-      this.topcards[1].title=rooms.length+''
-      this.totalRoomsCount=rooms.length
-    })
+  getRoomCount(): void {
+    this.service.getAllRooms().subscribe((rooms) => {
+      console.log(rooms.length);
+      this.topcards[1].title = rooms.length + "";
+      this.totalRoomsCount = rooms.length;
+    });
   }
   topcards: topcard[] = [
     {
       bgcolor: "success",
       icon: "bi bi-chat",
-      title: this.totalMessagesCount+'',
+      title: this.totalMessagesCount + "",
       subtitle: "Total Messages",
     },
     {
       bgcolor: "danger",
       icon: "bi bi-chat-text",
-      title: this.totalRoomsCount+'',
+      title: this.totalRoomsCount + "",
       subtitle: "Total Rooms",
     },
     {
