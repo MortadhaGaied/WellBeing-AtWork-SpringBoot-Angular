bd37b2efb66918e3c72f9f7a1be6718d9099c4a4
HomraniMahdi <mahdi.homrani@esprit.tn>
2022-05-05 02:17:34 +0100

    show

--- backoffice/src/app/collaboration/show-offers/show-offers.component.ts
+++ backoffice/src/app/collaboration/show-offers/show-offers.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { OfferService } from '../../Service/offer.service';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Offer } from '../../Models/Collaboration/offer';
 
 @Component({
   selector: 'app-show-offers',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-offers.component.scss']
 })
 export class ShowOffersComponent implements OnInit {
-
-  constructor() { }
+  Offer : Offer = new Offer;
+  constructor(private activatedRoute: ActivatedRoute,private dialog :MatDialog, router: Router,@Inject(OfferService)
+  private OfferService: OfferService , @Inject(MAT_DIALOG_DATA) public data:Offer) { }
 
   ngOnInit(): void {
+    this.OfferService.$eventEmit.subscribe((data:Offer)=> {
+      this.Offer=data;
+      this.getOffreById(data.idOffer);
+      console.log(this.Offer);
+    })
+  }
+  showCollaboration(){
+    this.OfferService.getOfferById(this.Offer.idOffer);
+  }
+  getOffreById(id : number){
+    this.OfferService.getOfferById(id).subscribe(offre=>console.log(offre))
   }
 
 }
